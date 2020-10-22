44c2e3f78001a81cb8c0c043099eba48d5766353
thurcst <ahac@cin.ufpe.br>
2020-10-22 00:52:20 -0300

    Starting new project

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
+
+export default function App(){
+
+  async function getLocationAsync() {
+    // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
+    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      return Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  }
+
+  async function checkPermissions(){
+    const { status, expires, permissions } = await Permissions.getAsync(Permissions.LOCATION);
+    if(status !== 'granted'){
+      alert('Hey! We need those permissions :(')
+    }
+  }
+
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.temp_text}>Barber</Text>
+      <Text style={styles.temp_text}>Shop</Text>
+      <StatusBar style="light"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#303030',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  temp_text:{
+    color: '#fff',
+    fontSize: 50,
+  },
+});
